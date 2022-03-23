1d7d188ac694a3b906950251b0d028d16489749b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-23 19:44:18 -0400

    added styles

--- js/js.js
+++ js/js.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 function mainDiv() {
     return `
         <ul class="nav--list">
-            <li class="list--items"><a href="#">About</a></li>
+            <li class="list--items"><a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" >About</a></li>
 
             <li class="list--items"><a href="#">Projects</a></li>
 
@@ -29,10 +29,10 @@ function header() {
             <h1>I'm <strong class="yellow">Uziel Carranza</strong></h1>
             <p class="header--para">A <strong class="yellow"> end software engineer</strong>
             with a strong passion for building responsive web application</p>
-            <p class="header--para">get to <a href="#" class="header--a">know me</a></p>
+            <p class="header--para">get to <a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" class="header--a">know me</a></p>
         </div>
         <div class="social--links">
-            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" ><i class="fa-brands fa-linkedin"></i></a>
             <a href="#"><i class="fa-brands fa-github"></i></a>
         </div>
     `
