da3975592534d4f39a18252337acd237ac26c55b
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-23 19:23:08 -0400

    started designed for mobile approach

--- js/js.js
+++ js/js.js
@@ -26,8 +26,8 @@ function header() {
     return `
         <div class="header--content">
             <h1>Hello There</h1>
-            <h1>I am <strong class="yellow">Uziel Carranza</strong></h1>
-            <p class="header--para">I am a <strong class="yellow">end software engineer</strong>
+            <h1>I'm <strong class="yellow">Uziel Carranza</strong></h1>
+            <p class="header--para">A <strong class="yellow"> end software engineer</strong>
             with a strong passion for building responsive web application</p>
             <p class="header--para">get to <a href="#" class="header--a">know me</a></p>
         </div>
