c21b1d500e9c7a2287c6446c7a425026404d8506
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-23 20:08:20 -0400

    added styles to links

--- js/js.js
+++ js/js.js
@@ -9,14 +9,15 @@ $(document).ready(function () {
 function mainDiv() {
     return `
         <ul class="nav--list">
-            <li class="list--items"><a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" >About</a></li>
+            <li class="list--items"><a href="https://www.linkedin.com/in/uziel-carranza-charro"
+                                       target="_blank">About</a></li>
 
             <li class="list--items"><a href="#">Projects</a></li>
 
             <li class="list--items"><a href="#">Contact</a></li>
 
-            <li class="list--items"><i class="fas fa-adjust"></i
-            ></li>
+            <li class="list--items"><a href=""><i class="fas fa-adjust"></i
+            ></a></li>
         </ul>
     `
 }
@@ -28,12 +29,17 @@ function header() {
             <h1>Hello There</h1>
             <h1>I'm <strong class="yellow">Uziel Carranza</strong></h1>
             <p class="header--para">A <strong class="yellow"> end software engineer</strong>
-            with a strong passion for building responsive web application</p>
-            <p class="header--para">get to <a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" class="header--a">know me</a></p>
+                with a strong passion for building responsive web application</p>
+            <p class="header--para">get to <a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank"
+                                              class="header--a">know me</a></p>
         </div>
         <div class="social--links">
-            <a href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank" ><i class="fa-brands fa-linkedin"></i></a>
-            <a href="#"><i class="fa-brands fa-github"></i></a>
+            <a class="link-element click" href="https://www.linkedin.com/in/uziel-carranza-charro" target="_blank"><i
+                    class="fa-brands fa-linkedin"></i></a>
+            <a class="link-element click" href="https://github.com/UzielCarranza" target="_blank"><i
+                    class="fa-brands fa-github"></i></a>
+            <a class="link-element click" href="#" target="_blank"><i class="fas fa-file-pdf"></i></a>
         </div>
     `
 }
+
