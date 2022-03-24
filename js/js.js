df34a889d2a8468b89105919cbf1a2cc5c9131f0
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-03-24 13:24:48 -0400

    set layout for desktop view

--- js/js.js
+++ js/js.js
@@ -100,17 +100,17 @@ function getLanSection() {
     return `
         <h1>Technologies</h1>
         <div class="row" id="technologies">
-            <div class="col-12 text-center">
-                <img class="col-2 tech-imgs ml-4" src="../img/html.png" alt="HTML language">
+            <div class="col-12  col-lg-12 text-center tech-imgs-section ">
+                <img class="col-2 col-lg-2 tech-imgs ml-4" src="../img/html.png" alt="HTML language">
             </div>
-            <div class="col-10 ml-4 m-4">
-                <img class="col-3 tech-imgs"src="../img/css3.png" alt="css language" style="height: 90%">
+            <div class="col-10 col-lg-12  ml-4 m-4 tech-imgs-section ">
+                <img class="col-3 col-lg-2 tech-imgs"src="../img/css3.png" alt="css language" style="height: 90%">
             </div>
-            <div class="col-10 d-flex justify-content-end">
-                <img class="col-2 tech-imgs" src="../img/js.png" alt="JavaScript language">
+            <div class="col-10 col-lg-12 d-flex justify-content-end tech-imgs-section ">
+                <img class="col-2 col-lg-2 tech-imgs" src="../img/js.png" alt="JavaScript language">
             </div>
-            <div class="col-11 text-center mt-4">
-                <img class="col-2 tech-imgs" src="../img/bootstrap.png" alt="bootStrap library">
+            <div class="col-11 col-lg-12 text-center mt-4 tech-imgs-section ">
+                <img class="col-2 col-lg-2 tech-imgs" src="../img/bootstrap.png" alt="bootStrap library">
             </div>
         </div>
 
